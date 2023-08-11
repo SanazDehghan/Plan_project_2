36228a438173eaf56698df54bfd4677887fe078a
Sanaz Dehghan <sanazdgh@gmail.com>
2023-08-11 18:05:21 +0330

    not complete the test

---
+++ src/modules/plan/program/program.spec.ts
@@ -0,0 +1,13 @@
+/* import { ProgramService } from "./program.service"
+
+describe("add proram", ()=>{
+    const program = new ProgramService
+    it("should not create program if user is not admin", ()=>{
+        expect (
+            program.addProgram(
+                {id:1, title:"test", description:" not important", deadline:new Date}, 
+                {title:"tarh1", description:"not important", },
+                {username:"number1", password:"number2", role:"Representative"})
+        ).toBe(false)
+    })
+}) */
